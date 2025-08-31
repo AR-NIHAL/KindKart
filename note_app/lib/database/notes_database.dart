1a6c244e43b740c24b458b8ec46c48b9e3da3836
Ashikur Rahman Nihal <arnihal.cs@gmail.com>
2025-08-31 21:37:13 +0600

    note
    Note

---
+++ note_app/lib/database/notes_database.dart
@@ -0,0 +1,34 @@
+import 'package:sqflite/sqflite.dart' as sqflite;
+import 'package:path/path.dart';
+
+class NotesDatabase {
+  static final NotesDatabase instance = NotesDatabase._init();
+  NotesDatabase._init();
+
+  static sqflite.Database? _database;
+
+  Future<sqflite.Database> _initDB(String filePath) async {
+    final dpPath = await sqflite.getDatabasesPath();
+    final path = join(dpPath, filePath);
+
+    return await sqflite.openDatabase(
+      path,
+      version: 1,
+      onCreate:
+      
+    );
+  }
+
+  Future _createDB(sqflite.Database db, int version) async{
+    await db.execute('''
+      CREATE TABLE notes)
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      description TEXT NOT NULL,
+      date TEXT NOT NULL,
+      color INTEGER NOT NULL DEFAULT 0
+      '''
+    );
+    
+  }
+}
---
+++ note_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(app)
+
+}
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes App',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey
+      ),
+      home: const,
+    );
+  }
+}
